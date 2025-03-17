e14a04fa929609378e6df0527c4463678b3c6336
annams <annaextrasteckel@gmail.com>
2025-03-17 10:01:56 -0700

    step 2 complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ import './App.css';
 import { useState } from 'react';
 
 const App = () => {
-    // Exercise 1: create state variable named team and set initial state to empty array []
+    // Step 1: create state variable named team and set initial state to empty array []
     const [team, setTeam] = useState([]);
+
+    // Step 2: create state variable named money and set initial state to 100
+    const [money, setMoney] = useState(100);
 
     return (
         <>
