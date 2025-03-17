8ce1cadeaf38ca224aa5747dab9c42b91c14d998
annams <annaextrasteckel@gmail.com>
2025-03-17 10:00:53 -0700

    step 1 complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 // src/App.jsx
 
+import './App.css';
+import { useState } from 'react';
+
 const App = () => {
+    // Exercise 1: create state variable named team and set initial state to empty array []
+    const [team, setTeam] = useState([]);
 
     return (
-        <h1>Hello world!</h1>
+        <>
+
+            <h1>Hello world!</h1>
+        
+        </>
     );
 }
 
-export default App
+export default App;
